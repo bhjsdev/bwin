ba192d5953d78aa49dd483e2afb2cfa7c07c15f6
Bhjs Dev <bhjsdev@gmail.com>
2024-11-29 14:39:47 +1100

    chore: update version to 0.1.0, update vite for lib build, and add a demo page

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: "./dev",
+  root: './dev',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    lib: {
+      entry: '../src/index.js',
+      formats: ['es'],
+      name: 'BinaryWindow',
+      fileName: 'bwin',
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name == 'style.css') return 'bwin.css';
+          return assetInfo.name;
+        },
+      },
+    },
+  },
 });
