b06a8e90e8a8fa848777602bbbc2a191066d8428
Oh Xyz <ohxyzcom@gmail.com>
2026-06-29 16:21:11 +1000

    refactor: use original pane's sash id as part of detached glass's DOM id

--- src/binary-window/glass/action.detach.js
+++ src/binary-window/glass/action.detach.js
@@ -7,19 +7,20 @@ export default {
   className: 'bw-action--detach',
   onClick: async (event, binaryWindow) => {
     const paneEl = event.target.closest('bw-pane');
+    const paneSashId = paneEl.getAttribute('sash-id');
     const glassEl = paneEl.querySelector('bw-glass');
-
     const windowRect = binaryWindow.windowElement.getBoundingClientRect();
     const width = windowRect.width - DETACHED_GLASS_INSET * 2;
     const height = windowRect.height - DETACHED_GLASS_INSET * 2;
+
     const detachedGlassEl = await binaryWindow.addDetachedGlass({
+      id: paneSashId + '-F',
       position: 'center',
       width,
       height,
       originalGlassElement: glassEl,
     });
 
-    const paneSashId = paneEl.getAttribute('sash-id');
     const paneSash = binaryWindow.rootSash.getById(paneSashId);
     const siblingSashId = paneSash.parent.getChildSiblingById(paneSashId).id;
 
